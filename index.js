092acd6b0df607459e0da638d3c2504e0a198fa3
Abraham <abukip344@gmail.com>
2024-03-25 10:45:01 -0400

    reduce lab

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+//reduce elements in batteryBatches to a single sum
+const totalBatteries = batteryBatches.reduce(function (acc, currentNumber){
+    return acc + currentNumber;
+}, 0)
